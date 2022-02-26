10e10c505a2983c6674961b6190097519ca00801
skv12 <sandal.skv12@gmail.com>
2022-02-26 13:18:46 +0900

    ionic start

---
+++ src/pages/mainActivity.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonItemDivider,
+  IonButton,
+} from '@ionic/react';
+import axios from 'axios';
+
+import ExploreContainer from '../components/ExploreContainer';
+import './mainActivity.css';
+import LoginActivity from './loginActivity';
+import Tab1 from './Tab1';
+import TabNavigator from '../components/tabNavigator';
+
+const MainActivity: React.FC = () => {
+    const [auth, setAuth] = useState(true);
+    if(!auth) return(
+        <LoginActivity/>
+    )
+    else return (
+      
+    <Tab1/>
+  );
+};
+
+export default MainActivity;
